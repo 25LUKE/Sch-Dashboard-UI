20e90337234e4ac6bff6ac547850c7cc2dc8e20c
25LUKE <ericluck25.com>
2024-11-03 20:11:59 +0100

    school-UI

---
+++ src/components/FinanceChart.tsx
@@ -0,0 +1,141 @@
+"use client"
+
+import Image from "next/image";
+
+import { 
+  LineChart, 
+  Line, 
+  XAxis, 
+  YAxis, 
+  CartesianGrid, 
+  Tooltip, 
+  Legend, 
+  ResponsiveContainer 
+} from 'recharts';
+
+const data = [
+  {
+    name: 'Jen',
+    income: 100,
+    expense: 2400,
+  },
+  {
+    name: 'Feb',
+    income: 75,
+    expense: 1398,
+  },
+  {
+    name: 'Mar',
+    income: 50,
+    expense: 9800,
+  },
+  {
+    name: 'Apr',
+    income: 25,
+    expense: 3908,
+  },
+  {
+    name: 'May',
+    income: 0,
+    expense: 4800,
+  },
+  {
+    name: 'Jun',
+    income: 2390,
+    expense: 3800,
+  },
+  {
+    name: 'Jul',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Aug',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Sep',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Oct',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Nov',
+    income: 3490,
+    expense: 4300,
+  },
+  {
+    name: 'Dec',
+    income: 3490,
+    expense: 4300,
+  },
+];
+
+const FinanceChart = () => {
+  return (
+    <div className="bg-white rounded-xl p-4 h-full w-full">
+      <div className="flex justify-between items-center">
+            <h1 className='text-lg font-semibold'>Finances</h1>
+            <Image src="/finance.png" alt='' width={20} height={20}/>
+        </div>
+        <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+        >
+          <CartesianGrid 
+            strokeDasharray="3 3" 
+            vertical={false} 
+            stroke='#ddd'
+          />
+          <XAxis 
+            dataKey="name" 
+            axisLine={false} 
+            tick={{fill:"#d1d5db"}} 
+            tickLine={false}
+            tickMargin={10}
+          />
+          <YAxis 
+            axisLine={false} 
+            tick={{ fill: "#d1d5bd"}} 
+            tickLine={false}
+            tickMargin={20}
+          />
+          <Tooltip 
+            contentStyle={{
+              borderRadius: "10px", 
+              borderColor:"lightgray"
+            }}/>
+          <Legend 
+            align='center' 
+            verticalAlign='top' 
+            wrapperStyle={{ 
+              paddingTop:"10px", 
+              paddingBottom:"30px"
+            }}
+          />
+          <Line 
+            type="monotone" 
+            dataKey="income" 
+            stroke="#C3EBFA"
+            strokeWidth={5}
+          />
+          <Line 
+            type="monotone" 
+            dataKey="expense" 
+            stroke="#CFCEFF"
+            strokeWidth={5}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
+
+export default FinanceChart;
